ca5dfaade84ff1c6d847e507777ec1f1f1605e7d
FacelessObserver <semabarbos@gmail.com>
2025-09-11 10:08:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,7 @@ void zad_Iosifa(int n, int m)
      */
     vector<int> winners;
     for (int step = 2; step <= m; step++)
-        {
-        cout << "Игра номер " << step - 1 << "\nШаг: " << step << endl;
-
+    {
         vector<int> nums; // Номера участников
         for (int i = 1; i <= n; i++)
         {
@@ -26,26 +24,11 @@ void zad_Iosifa(int n, int m)
         for (int i = 0; i < n-1; i++) // Пока не останется один участник
         {
             pointer = (pointer + step - 1) % nums.size(); // Двигаем указатель на шаг
-
             int gotcha = nums[pointer]; // Невезучий номер
-            cout << "Номер удаляемого участника: " << gotcha << endl;
-
             nums.erase(nums.begin() + pointer); // Исключение выбывшего
-
-            cout << "Оставшиеся номера: ";
-            for (int j = 0; j < nums.size(); j++)
-            {
-                cout << nums[j] << " ";
-            }
-            cout << "\n";
         }
-        cout << "Выиграл номер: " << nums[0] << "\n" << endl;
+        cout << "При m = " << step << " выиграл номер: " << nums[0] << "\n" << endl;
         winners.push_back(nums[0]);
-    }
-    cout << "Победители раундов:" << endl;
-    for (int i = 0; i < winners.size(); i++)
-    {
-        cout << winners[i] << " ";
     }
 }
 
